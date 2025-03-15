5a92dca087b0258fa172f712e8e40abb56b6d5dd
MidhunKV79 <midhunkv79@gmail.com>
2025-03-15 14:58:10 +0530

    first commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+export const Contact = (props) => {
+  return (
+    <div>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Contact Us</h2>
+                <p>
+                  Feel free to reach out to us using the contact information below.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 contact-info">
+            <div className="contact-item">
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Address
+                </span>
+                {props.data ? props.data.address : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Phone
+                </span>
+              </p>
+              <ul>
+                {props.data && props.data.phone
+                  ? props.data.phone.map((num, index) => <li key={index}>{num}</li>)
+                  : "loading"}
+              </ul>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
+              </p>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="row">
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.facebook : "/"}>
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.twitter : "/"}>
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.youtube : "/"}>
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            &copy; 2025 Mangalasseril Construction. All rights reserved
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
